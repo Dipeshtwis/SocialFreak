5079ef1a37f5b00db03325a3ce14f2b3cbb20c51
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-11-13 17:00:19 +0530

    Save user data

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,13 @@
 class SessionsController < ApplicationController
   
   def create
-  	render text: request.env['omniauth.auth'].to_yaml
+  	begin
+      @user = User.from_omniauth(request.env['omniauth.auth'])
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome, #{@user.name}!"
+    rescue
+      flash[:warning] = "There was an error while trying to authenticate you..."
+    end
+    redirect_to root_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  
+  class << self
+	def from_omniauth(auth_hash)
+	  user = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
+	  user.name = auth_hash['info']['name']
+	  user.location = auth_hash['info']['location']
+	  user.image_url = auth_hash['info']['image']
+	  user.url = auth_hash['info']['urls']['Twitter']
+	  user.save!
+	  user
+	end
+  end
 end
